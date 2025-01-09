56a7d1d518719ec963cb4d6ecd2cc2364ebb64b8
Sourav Das <iamsouravdaswade420@gmail.com>
2025-01-09 18:10:25 +0530

    Adeded profile image update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -99,8 +99,30 @@ const Profile = () => {
     }
 
 
-    const imagePatchHandler = async () =>{
-    //     TODO: add image patch request here.
+    const imagePatchHandler = async () => {
+        //     TODO: add image patch request here.
+        //     payload
+        const payload = {
+            profile_img: profileImage
+        }
+
+       try {
+            const URL = `${ROOT_URL}profile/update/`
+            const res = await axios.patch(URL,payload,{
+                headers:{
+                    Authorization:`Bearer ${userInfo.token}`,
+                    "Content-Type":"multipart/form-data"
+                }
+            })
+
+           console.table(res.data)
+
+       } catch (e){
+            console.log(e)
+
+       }
+
+
     }
 
     const imageChangeHandler = (e) => {
@@ -109,83 +131,83 @@ const Profile = () => {
 
     }
 
-        useEffect(() => {
-            fetchBoardDetails()
-            fetchUserProfileInfo()
+    useEffect(() => {
+        fetchBoardDetails()
+        fetchUserProfileInfo()
 
 
-        }, [userInfo]);
+    }, [userInfo]);
 
 
-        if (loading || boardLoading || profileLoading) {
-            return <MainLoader/>
-        }
-        if (err || boardError || profileError) {
-            return <h1>{err}</h1>
-        }
+    if (loading || boardLoading || profileLoading) {
+        return <MainLoader/>
+    }
+    if (err || boardError || profileError) {
+        return <h1>{err}</h1>
+    }
 
 
-        return (<div className={"wrapper__div"}>
-            {/*IT will redirect profile to profile/saved/ */}
-            {/*<Navigate to={"saved/"}/> */}
+    return (<div className={"wrapper__div"}>
+        {/*IT will redirect profile to profile/saved/ */}
+        {/*<Navigate to={"saved/"}/> */}
 
-            <div className="profile__container">
-                <div className="profile__section">
-                    {profileInfo ? <img src={profileInfo.profile_img} alt="#" className="profile__img"/> :
-                        <img src={PROFILE_IMG} alt="#" className="profile__img"/>}
+        <div className="profile__container">
+            <div className="profile__section">
+                {profileInfo ? <img src={profileInfo.profile_img} alt="#" className="profile__img"/> :
+                    <img src={PROFILE_IMG} alt="#" className="profile__img"/>}
 
-                    <h1>{userInfo?.username}</h1>
-                    <p>{userInfo?.email}</p>
-                    {profileInfo && <p className={"profile__desc"}>{profileInfo.desc}</p>}
-                    <input type="file" accept={"image/*"} onChange={imageChangeHandler}/>
-                    <button onClick={imagePatchHandler}>Save image</button>
-                    {/*<button onClick={removeUser}>Clean user Redux store </button>*/}
-                </div>
+                <h1>{userInfo?.username}</h1>
+                <p>{userInfo?.email}</p>
+                {profileInfo && <p className={"profile__desc"}>{profileInfo.desc}</p>}
+                <input type="file" accept={"image/*"} onChange={imageChangeHandler}/>
+                <button onClick={imagePatchHandler}>Save image</button>
+                {/*<button onClick={removeUser}>Clean user Redux store </button>*/}
             </div>
-            <div className="saved__created__toggle__container">
-                {/*TODO: Add navlink instead of link */}
-                <NavLink to={"created/"}
-                         className={({isActive}) => (isActive ? "navlink__active" : "navlink")}>Created</NavLink>
-                <NavLink to={"saved/"}
-                         className={({isActive}) => (isActive ? "navlink__active" : "navlink")}>Saved</NavLink>
+        </div>
+        <div className="saved__created__toggle__container">
+            {/*TODO: Add navlink instead of link */}
+            <NavLink to={"created/"}
+                     className={({isActive}) => (isActive ? "navlink__active" : "navlink")}>Created</NavLink>
+            <NavLink to={"saved/"}
+                     className={({isActive}) => (isActive ? "navlink__active" : "navlink")}>Saved</NavLink>
+        </div>
+
+        <div className="create__btn__container ">
+            <div className={`create__btn ${modalShow && "active"}`}
+                 onClick={() => setModalShow(prevState => !prevState)}>
+
+                <FaPlus/>
             </div>
 
-            <div className="create__btn__container ">
-                <div className={`create__btn ${modalShow && "active"}`}
-                     onClick={() => setModalShow(prevState => !prevState)}>
-
-                    <FaPlus/>
-                </div>
-
-                {modalShow && <div className="modal">
-                    <span>Create</span>
-                    <Link to={"/create"} className="modal_title">
-                        Pin
-                    </Link>
-                    <span className="modal_title" onClick={modalToggleHandler}>
+            {modalShow && <div className="modal">
+                <span>Create</span>
+                <Link to={"/create"} className="modal_title">
+                    Pin
+                </Link>
+                <span className="modal_title" onClick={modalToggleHandler}>
                         Board
                 </span>
-                </div>}
-
-
-            </div>
-
-
-            <Outlet/>
-
-            {/* if location pathname is profile i want to show the boards if changed to boards the boards will be there if created path the created pins will be there*/}
-            {location.pathname === "/profile" && <div className="boards__container">
-
-
-                {boardInfo.map(board => (
-                    <Board name={board.name} key={board.id} pins={board.pins} boardId={board.id}/>))}
-
             </div>}
 
-            {boardModalToggle && <BoardCreateModal toggle={boardModalToggle} toggleFun={modalToggleHandler}/>}
+
+        </div>
 
 
-        </div>);
-    };
+        <Outlet/>
 
-    export default Profile;
+        {/* if location pathname is profile i want to show the boards if changed to boards the boards will be there if created path the created pins will be there*/}
+        {location.pathname === "/profile" && <div className="boards__container">
+
+
+            {boardInfo.map(board => (
+                <Board name={board.name} key={board.id} pins={board.pins} boardId={board.id}/>))}
+
+        </div>}
+
+        {boardModalToggle && <BoardCreateModal toggle={boardModalToggle} toggleFun={modalToggleHandler}/>}
+
+
+    </div>);
+};
+
+export default Profile;
