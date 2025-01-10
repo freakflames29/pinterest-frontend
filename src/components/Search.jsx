1430cc4e4722be2a29ac4ace130a555a60e91da3
Sourav Das <iamsouravdaswade420@gmail.com>
2025-01-10 18:34:43 +0530

    added iamge search option

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams, useSearchParams} from "react-router-dom";
+import MainLoader from "./MainLoader.jsx";
+import {ROOT_URL} from "../Constants.js";
+import axios from "axios";
+import ImageGrid from "./ImageGrid.jsx";
 
 const Search = () => {
     const [params] = useSearchParams()
     const query = params.get("search")
+    const [searchPins, setSearchPins] = useState([])
+    const [loader, setLoader] = useState(false)
+    const [err, setErr] = useState(null)
 
+    const getSearchPins = async () => {
+
+        try {
+            setLoader(true)
+            const URL = `${ROOT_URL}pin?search=${query}`
+            const res = await axios(URL)
+            console.log(res.data)
+            if (res.data.length > 0) {
+
+                setSearchPins(res.data)
+                setErr(null)
+            }
+            else{
+                setErr("No pins found")
+            }
+
+        } catch (e) {
+            console.log(e)
+            setErr(e.message)
+        } finally {
+            setLoader(false)
+        }
+
+    }
+
+    useEffect(() => {
+        getSearchPins()
+    }, [query]);
+
+    if (loader) {
+        return <MainLoader/>
+    }
+    if (err) {
+        return <h1 align={"center"}>{err}</h1>
+    }
 
     return (
         <div>
-            <h1>Searched this -> {query}</h1>
+            <ImageGrid pinInfo={searchPins}/>
         </div>
     );
 };
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,25 +3,34 @@ import {FaSearch} from "react-icons/fa";
 import axios from "axios";
 import {ROOT_URL} from "../Constants.js";
 import {Link, Navigate, useNavigate} from "react-router-dom"
+
 const SearchBar = () => {
 
     const [searchItem, setSearchItem] = useState("")
     const [searchData, setSearchData] = useState([])
     const navigate = useNavigate()
+    const controller = new AbortController()
+    const signal = controller.signal
 
     const searchHandler = (e) => {
         setSearchItem(e.target.value)
-        console.log(e.target.value)
-        if (e.target.value.length <= 0) {
-            setSearchItem("")
+        if (e.keyCode === 13) {
+            controller.abort()
             setSearchData([])
+            navigate(`/pin?search=${searchItem}`)
+        } else {
+
+
+            if (e.target.value.length <= 0) {
+                setSearchItem("")
+                setSearchData([])
+            }
         }
     }
 
 
     useEffect(() => {
-        const controller = new AbortController()
-        const signal = controller.signal
+
         const searchFun = async () => {
 
             try {
@@ -48,43 +57,45 @@ const SearchBar = () => {
     }, [searchItem]);
 
 
-    const sendToPin = (pinId)=>{
+    const sendToPin = (pinId) => {
         setSearchData([])
-            // window.location=`/pin/${pinId}`
+        // window.location=`/pin/${pinId}`
         navigate(`/pin/${pinId}`)
     }
-    return (
-        <>
-            <div className="searchBar">
+    const removeSearchResultBox = () => {
+        if (searchData.length > 0) {
+            // if there is search element in searchbox then wait 500ms to disappear the box so that we can click on the search element
+            setTimeout(() => setSearchData([]), 500)
+        }
 
-                <FaSearch/>
+    }
+    return (<>
+        <div className="searchBar">
 
-                <input type="text" placeholder={`Search your ideas here...`} className="input__field" value={searchItem}
-                       onChange={searchHandler}/>
-            </div>
+            <FaSearch/>
 
-            {/*Change search list*/}
-            {searchData.length > 0 &&
-                <div className="search__item__list">
-                    {searchData.map((item) => <>
+            <input type="text" placeholder={`Search your ideas here...`} className="input__field" value={searchItem}
+                   onChange={searchHandler} onBlur={removeSearchResultBox} onKeyDown={searchHandler}/>
+        </div>
 
-                                <div className="search__result" key={item.id} onClick={()=>sendToPin(item.id)}>
+        {/*Change search list*/}
+        {searchData.length > 0 && <div className="search__item__list">
+            {searchData.map((item) => <div key={item.id}>
 
-
-                                    <FaSearch/>
-                                    <span>{item.title}</span>
-
-                                </div>
+                <div className="search__result" onClick={() => sendToPin(item.id)}>
 
 
-                        </>
-                    )}
-
+                    <FaSearch/>
+                    <span>{item.title}</span>
 
                 </div>
-            }
-        </>
-    );
+
+
+            </div>)}
+
+
+        </div>}
+    </>);
 };
 
 export default SearchBar;
